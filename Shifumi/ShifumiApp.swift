a11a7c4f0653cc327a329a3a7e852c724ad025bc
Alkanife <90835925+alkanife@users.noreply.github.com>
2023-07-07 14:59:21 +0200

    Initial Commit

---
+++ Shifumi/ShifumiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShifumiApp.swift
+//  Shifumi
+//
+//  Created by Arthur Beau on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShifumiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
